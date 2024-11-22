d6fe6701981673cf46ea0ffc91e79dc625a13a24
realmerona <143993181+realmerona@users.noreply.github.com>
2024-11-22 11:58:02 +0900

    Add files via upload

---
+++ swproject/src/main/java/org/zerock/jdbcex/entity/InterviewRepository.java
@@ -0,0 +1,9 @@
+package org.zerock.jdbcex.entity;
+
+import org.zerock.jdbcex.entity.Interview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+}
